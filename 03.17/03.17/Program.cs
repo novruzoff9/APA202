be054706db0a90fc60c31b40d69a387c8f8f6f6b
User <yagmurnov2005@gmail.com>
2026-03-19 14:57:50 +0400

    Completed 03.17 tasks

--- 03.17/03.17/Program.cs
+++ 03.17/03.17/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _03._17;
+
+//Vehicle vehicle1 = new();
+////vehicle1.Brand = "a";
+//vehicle1._brand = "a";
+//Console.WriteLine(vehicle1.Brand);
+
+Vehicle vehicle1 = new("Audi", "RS6", 2015);
+vehicle1.StartEngine();
+vehicle1.Drive(50);
+vehicle1.VehicleInfo();
---
+++ 03.17/03.17/Vehicle.cs
@@ -0,0 +1,80 @@
+﻿namespace _03._17;
+
+internal class Vehicle
+{
+    private string _brand;
+    public string Brand
+    {
+        get
+        {
+            return _brand;
+        }
+        set
+        {
+            if(value.Length >= 3)
+                _brand = value;
+            else
+                Console.WriteLine("Minimum 3 simvol olmalidir");
+        }
+    }
+
+    private string _model;
+    public string Model
+    {
+        get
+        {
+            return _model;
+        }
+        set
+        {
+            if (value.Length >= 3)
+                _model = value;
+            else
+                Console.WriteLine("Minimum 3 simvol olmalidir");
+        }
+    }
+
+    private int _year;
+    public int Year
+    {
+        get
+        {
+            return _year;
+        }
+        set
+        {
+            if (value >= 1900)
+                _year = value;
+            else
+                Console.WriteLine("Minimum il 1900 olmalidir");
+        }
+    }
+
+    public int MileageKm { get; set; }
+    public bool IsRunning { get; set; }
+    public Vehicle(string brand, string model, int year)
+    {
+        Brand = brand;
+        Model = model;
+        Year = year;
+    }
+
+    public void StartEngine() => IsRunning = true;
+    public void StopEngine() => IsRunning = false;
+    public void Drive(int km)
+    {
+        if (IsRunning)
+            MileageKm += km;
+        else
+            Console.WriteLine("Muherrik islek deyil");
+    }
+
+    public void VehicleInfo()
+    {
+        Console.WriteLine($"Brand {Brand}");
+        Console.WriteLine($"Model {Model}");
+        Console.WriteLine($"Year {Year}");
+        Console.WriteLine($"Mileage {MileageKm}km");
+        Console.WriteLine($"Running {IsRunning}");
+    }
+}
